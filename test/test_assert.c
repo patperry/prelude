9586db972f29ffb31fde1286901349fc97d0dc9c
Patrick Perry <patperry@gmail.com>
2019-09-04 19:32:06 -0400

    more assertion tests

--- test/test_assert.c
+++ test/test_assert.c
@@ -37,6 +37,26 @@ static void unit_notPanic(void) {
     Assert_NotPanic(catch_panic, NULL);
 }
 
+static void fail_assert(void *arg) {
+    (void)arg;
+    Assert(False);
+}
+
+static void unit_assert(void) {
+    Assert_Panic(fail_assert, NULL);
+}
+
+static void fail_errorNone(void *arg) {
+    (void)arg;
+    Error err;
+    Error_New(&err, S("error"));
+    Assert_ErrorNone(&err);
+}
+
+static void unit_errorNone(void) {
+    Assert_Panic(fail_errorNone, NULL);
+}
+
 static void fail_stringEq(void *arg) {
     (void)arg;
     Assert_StringEq(S("a"), S("b"));
@@ -44,6 +64,15 @@ static void fail_stringEq(void *arg) {
 
 static void unit_stringEq(void) {
     Assert_Panic(fail_stringEq, NULL);
+}
+
+static void fail_stringNotEq(void *arg) {
+    (void)arg;
+    Assert_StringNotEq(S(""), S(""));
+}
+
+static void unit_stringNotEq(void) {
+    Assert_Panic(fail_stringNotEq, NULL);
 }
 
 int main(int argc, const char **argv) {
@@ -56,7 +85,10 @@ int main(int argc, const char **argv) {
     Int g = TestSuite_Group(&s, S("assert"));
     TestSuite_AddUnit(&s, g, S("panic"), unit_panic);
     TestSuite_AddUnit(&s, g, S("not panic"), unit_notPanic);
+    TestSuite_AddUnit(&s, g, S("assert"), unit_assert);
+    TestSuite_AddUnit(&s, g, S("error none"), unit_errorNone);
     TestSuite_AddUnit(&s, g, S("string eq"), unit_stringEq);
+    TestSuite_AddUnit(&s, g, S("string not eq"), unit_stringNotEq);
 
     int ret = Test_Main(argc, argv, &s);
     Close();
